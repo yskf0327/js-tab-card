4674448c685838ecc8a0c9f49d6c365d7f2e40ea
Yusuke <ysk2645j4@gmail.com>
2025-10-30 16:16:30 +0900

    一旦完了

---
+++ vue-tab-card/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <div class="wrapper">
+      <ul class="card-wrapper">
+        <SpotCard
+        v-for="(spot,index) in spots"
+        :key="spot.name"
+        :spot="spot"
+        :id="index+1"
+        ></SpotCard>
+      </ul>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import {spots} from './data/spots';
+import SpotCard from './components/spotCard.vue';
+</script>
---
+++ vue-tab-card/src/components/SpotCard.vue
@@ -0,0 +1,58 @@
+<template>
+  <li class="card">
+    <div class="tab-button-wrapper" role="tablist">
+      <button v-for="tab in tabs" :key="tab.key" class="tab-button" role="tab" :aria-controls="tab.key + id"
+        :aria-selected="currentTab === tab.key" :class="{ active: currentTab === tab.key }"
+        @click="currentTab = tab.key">
+        {{ tab.label }}
+      </button>
+    </div>
+
+    <p class="spot-name">{{ spots.name }}</p>
+    <!-- photo -->
+    <div class="card-panel-wrapper">
+      <div class="card-panel" :id="`photo${id}`" role="tabpanel" :aria-labelledby="`photo${id}`"
+        v-show="currentTab === 'photo'">
+        <img :src="spots.photo" :alt="spot.name" class="spot-img" width="200" height="200">
+
+      </div>
+      <!-- info -->
+      <div class="card-panel" :id="`info${id}`" role="tabpanel" :aria-labelledby="`info${id}`"
+        v-show="currentTab === 'info'">
+        <p class="spot-desc">{{ spots.info }}</p>
+
+      </div>
+      <!-- Access -->
+      <div class="card-panel" :id="`access${id}`" role="tabpanel" :aria-labelledby="`access${id}`"
+        v-show="currentTab === 'access'">
+        <div class="spot-access">
+          <p class="spot-address">{{ spot.address }}</p>
+          <p class="spot-route">{{ spot.access }}</p>
+        </div>
+
+        <div class="spot-map" v-if="spot.map">
+          <iframe :title="spot.name" :src="spot.map" width="400" height="300" style="border:0" allowfullscreen
+            loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+      </div>
+    </div>
+
+  </li>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  spot: Object,
+  id: Number
+})
+
+const currentTab = ref('photo')
+
+const tabs = [
+  { key: 'photo', label: 'Photo' },
+  { key: 'info', label: 'Info' },
+  { key: 'access', label: 'Access' }
+]
+</script>
